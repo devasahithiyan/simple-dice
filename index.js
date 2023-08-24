0c26a4608545a728071437da2cf2059938fd0291
deva sahithiyan <devasahithiyan@gmail.com>
2023-08-24 20:10:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var name = prompt("Enter player 1 name");
+var name2 = prompt("Enter player 2 name");
+
+var randomvariable = Math.floor(Math.random() * 6) + 1;
+var diceimage = "dice"+randomvariable+".png";
+
+var imagesource = "images/"+diceimage;
+
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", imagesource);
+
+
+var randomvariable2 = Math.floor(Math.random()*6)+1;
+
+var diceimage2= "dice"+randomvariable2+".png";
+
+
+var randomimage2 = "images/"+diceimage2;
+
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomimage2);
+
+
+if(randomvariable > randomvariable2){
+  document.querySelector("h1").innerHTML = "Player 1 wins"
+} else if( randomvariable < randomvariable2){
+    document.querySelector("h1").innerHTML = "Player 2 wins"
+} else {
+    document.querySelector("h1").innerHTML = "Draw"
+}
